14d89b49e2e2457aae22a1973000ceab2bd9814e
Jonathan <colvilledesigns@gmail.com>
2019-11-06 15:34:21 -0600

    Assignment 13 completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
   function onReady() {
-    const toDos = [];
+    let toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
-
+    let id = 0;
 
 
     function renderTheUI(){
@@ -13,11 +13,21 @@
         const newLi = document.createElement('li');
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener('click', event => {
+          toDos = toDos.filter(function(item){
+            return item.id !== toDo.id;
+          })
+          renderTheUI();
+        });
 
         newLi.textContent = toDo.title;
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
+        newLi.appendChild(deleteBtn);
       })
     }
 
@@ -27,8 +37,10 @@
 
       toDos.push({
         title: newToDoText.value,
-        complete: false
+        complete: false,
+        id: id
       });
+      id++;
       newToDoText.value = '';
 
       renderTheUI(); //calls on the renderTheUI function above
